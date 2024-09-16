7c8d789b9bbbd092451ca8305e405d2c0065cb18
oiyl <68357819+oiyl@users.noreply.github.com>
2024-09-16 00:27:01 -0400

    dllmain console debug check

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -3,52 +3,55 @@
 #include <iostream>
 #include <thread>
 
-auto open_console() -> void {
-	AllocConsole();
+#ifdef DEBUG
+auto open_console ( ) -> void {
+	AllocConsole ( );
 
 	FILE* fp;
-	freopen_s(&fp, "CONOUT$", "w", stdout);
-	freopen_s(&fp, "CONOUT$", "w", stderr);
-	freopen_s(&fp, "CONIN$", "r", stdin);
+	freopen_s ( &fp, "CONOUT$", "w", stdout );
+	freopen_s ( &fp, "CONOUT$", "w", stderr );
+	freopen_s ( &fp, "CONIN$", "r", stdin );
 
-	SetConsoleTitle(L"debug console");
+	SetConsoleTitle ( L"debug console" );
 }
 
-auto close_console() -> void {
-	fclose(stdin);
-	fclose(stdout);
-	fclose(stderr);
+auto close_console ( ) -> void {
+	fclose ( stdin );
+	fclose ( stdout );
+	fclose ( stderr );
 
-	FreeConsole();
+	FreeConsole ( );
 }
+#endif
 
 auto WINAPI inject ( LPVOID lp_param ) -> DWORD {
 	auto mod = static_cast< HMODULE > ( lp_param );
-
-	open_console();
-	
+#ifdef DEBUG
+	open_console ( );
+#endif
 	hooks::init ( );
 
 	while ( !GetAsyncKeyState ( VK_END ) ) {
 		std::this_thread::sleep_for ( std::chrono::milliseconds ( 100 ) );
 
 		/* fix bugged camera */
-		if (GetAsyncKeyState(VK_F6)) {
-			hooks::unload();
-			/*  weird game edge case where it gets messed up i guess */
-			std::this_thread::sleep_for(std::chrono::seconds(5));
+		if ( GetAsyncKeyState ( VK_F6 ) ) {
+			hooks::unload ( );
+			/* weird game edge case where it gets messed up i guess */
+			std::this_thread::sleep_for ( std::chrono::seconds ( 5 ) );
 
-			hooks::init();
+			hooks::init ( );
 		}
 	}
-
 
 	FreeLibraryAndExitThread ( mod, 0 );
 }
 
 auto uninject ( ) -> void {
 	hooks::unload ( );
-	close_console();
+#ifdef DEBUG
+	close_console ( );
+#endif
 }
 
 BOOL APIENTRY DllMain ( HMODULE mod, DWORD reason, LPVOID reserved ) {
